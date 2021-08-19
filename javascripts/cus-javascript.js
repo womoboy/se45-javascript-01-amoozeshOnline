f2247b10bc6f554d0594dd731009ee804c5856b7
reza2boyce <reza2boyce@gmail.com>
2021-08-20 00:42:25 +0430

    tamrin dom creat,replace,remove element ba javascript

--- javascripts/cus-javascript.js
+++ javascripts/cus-javascript.js
@@ -1,21 +1,13 @@
-//add before, replace, delete---elements with javascript
 var aTag = document.createElement('a');
-var aTagText = document.createTextNode('aAtag');
-aTag.appendChild(aTagText);
-aTag.setAttribute('name', 'aTag-name');
+var aText = document.createTextNode('Salam');
+aTag.appendChild(aText);
 
 var bTag = document.createElement('a');
-var bTagText = document.createTextNode('btag');
-bTag.appendChild(bTagText);
-bTag.setAttribute('name', 'bTag-name');
+var bText = document.createTextNode('Chetori');
+bTag.appendChild(bText);
 
 var topDiv = document.getElementById('top-div');
-var aContact = topDiv.children[2];
-var aAbout = topDiv.children[1];
-//topDiv.insertBefore(aTag , aContact);
-//topDiv.replaceChild(aTag, aContact);
-//topDiv.removeChild(aContact);
-//topDiv.removeChild(aContact);
-//topDiv.replaceChild(aTag , aAbout);
-//topDiv.appendChild(bTag);
-
+topDiv.appendChild(aTag);
+topDiv.appendChild(bTag);
+//topDiv.replaceChild(bTag, aTag);
+//topDiv.removeChild(bTag);
