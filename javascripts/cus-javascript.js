b54baf210af5aab4cb60316f117c830c4c15d21f
reza2boyce <reza2boyce@gmail.com>
2021-08-19 19:40:17 +0430

    ezafe,replace va remove kardane element ha tavassote javascript

--- javascripts/cus-javascript.js
+++ javascripts/cus-javascript.js
@@ -1,8 +1,21 @@
+//add before, replace, delete---elements with javascript
 var aTag = document.createElement('a');
-var aText = document.createTextNode('Inja click koni miri google');
-aTag.appendChild(aText);
-aTag.setAttribute('href', 'http://google.com');
-aTag.setAttribute('id', 'google-links');
+var aTagText = document.createTextNode('aAtag');
+aTag.appendChild(aTagText);
+aTag.setAttribute('name', 'aTag-name');
+
+var bTag = document.createElement('a');
+var bTagText = document.createTextNode('btag');
+bTag.appendChild(bTagText);
+bTag.setAttribute('name', 'bTag-name');
 
 var topDiv = document.getElementById('top-div');
-topDiv.appendChild(aTag);
+var aContact = topDiv.children[2];
+var aAbout = topDiv.children[1];
+//topDiv.insertBefore(aTag , aContact);
+//topDiv.replaceChild(aTag, aContact);
+//topDiv.removeChild(aContact);
+//topDiv.removeChild(aContact);
+//topDiv.replaceChild(aTag , aAbout);
+//topDiv.appendChild(bTag);
+
