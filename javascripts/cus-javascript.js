c6f94a0d79a3f895efaec37d42972423a458f33f
reza2boyce <reza2boyce@gmail.com>
2021-08-21 22:52:29 +0430

    tamrin dom inputText/Button Events again

--- javascripts/cus-javascript.js
+++ javascripts/cus-javascript.js
@@ -1,11 +1,11 @@
 function onClickText () {
-    var input = document.getElementById('input-01');
-    var pPar = document.createElement('p');
-    var pText = document.createTextNode(input.value);
-    pPar.appendChild(pText);
+     var input = document.getElementById('input-01');
+     var pPar = document.createElement('p');
+     var pText = document.createTextNode(input.value);
+     pPar.appendChild(pText);
 
-    var topDiv = document.getElementById('top-div');
-    topDiv.appendChild(pPar);
-    input.value = null;
-    input.focus();
+     var topDiv = document.getElementById('top-div');
+     topDiv.appendChild(pPar);
+     input.value = null;
+     input.focus();
 }
