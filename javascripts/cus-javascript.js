e539772ced4298c2d9312dda0feae5ecad4dffc9
reza2boyce <reza2boyce@gmail.com>
2021-08-23 14:06:10 +0430

    tamrin daryaft 2 input az noe adadi va hasel jame 2 taye anha ba feshordane click

--- javascripts/cus-javascript.js
+++ javascripts/cus-javascript.js
@@ -1,11 +1,16 @@
-function onClickText () {
-     var input = document.getElementById('input-01');
-     var pPar = document.createElement('p');
-     var pText = document.createTextNode(input.value);
-     pPar.appendChild(pText);
 
-     var topDiv = document.getElementById('top-div');
-     topDiv.appendChild(pPar);
-     input.value = null;
-     input.focus();
+
+function actionPluse (){
+    var inputA = document.getElementById('input-01');
+    var inputB = document.getElementById('input-02');
+    var eText = document.createTextNode((Number(inputA.value)) + (Number(inputB.value)));
+    var btn = document.getElementById('btn-01');
+    var exitP = document.getElementById('text-exit');
+
+    exitP.style.display = 'block';
+    exitP.textContent = null;
+    exitP.appendChild(eText);
+    inputA.Value = null;
+    inputB.Value = null;
+    inputA.focus();
 }
