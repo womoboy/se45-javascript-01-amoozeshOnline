b3191547b48747533043f56905d3b6bd94b42a94
reza2boyce <reza2boyce@gmail.com>
2021-08-25 21:50:26 +0430

    turnOn/turnOff light Tutorial

---
+++ javascripts/cus-javascript.js
@@ -0,0 +1,10 @@
+var btnOn = document.getElementById('btn-on');
+var btnOff = document.getElementById('btn-off');
+var light = document.getElementById('div-light');
+
+function lightOn (){
+    light.style.backgroundColor="yellow";
+}
+function lightOff (){
+    light.style.backgroundColor= null;
+}
