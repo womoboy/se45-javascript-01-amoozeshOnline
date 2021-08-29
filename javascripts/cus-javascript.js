70dbcec8cd7928d2aadd89d2ac57d523fb9693f9
reza2boyce <reza2boyce@gmail.com>
2021-08-29 20:51:54 +0430

    in backsh kheyli mohemeh az string method ha estefade shode bar roye tamrine ghabli *****kheyli moheme peyda kardane jaygahe horoof dar yek reshte

--- javascripts/cus-javascript.js
+++ javascripts/cus-javascript.js
@@ -1,10 +1,18 @@
 var $pInfo = document.getElementById('text-info');
 var $firstName = document.getElementById('first-name');
+var $textPriority = document.getElementById('text-priority');
 
 function printText() {
     $pInfo.style.visibility = "visible";
-    $pInfo.innerHTML = "your name is: " + $firstName.value;
+    $pInfo.innerHTML = $firstName.value;
 }
+
 function replaceName(){
     $pInfo.innerHTML = $pInfo.innerHTML.replace(/reza/i, "Sara");
+}
+
+function priorityAction(){
+    $textPriority.style.visibility = "visible";
+    $textPriority.innerHTML = $pInfo.innerHTML.indexOf('e');
+    //$textPriority.innerHTML = $pInfo.innerHTML.charAt(2);
 }
